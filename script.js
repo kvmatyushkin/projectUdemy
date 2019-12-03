fd374552e7c99ec88a6874246f8ebd94b6970b05
Kostya <kvmatyushkin@gmail.com>
2019-12-03 14:17:29 +0300

    Redid expenses

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 let money = prompt('Ваш бюджет на месяц?', ''),
-    time = prompt('Введите дату в формате YYYY-MM-DD', ''),
-    necessarily = prompt('Введите обязательную статью расходов в этом месяц', ''),
-    necessarilyMoney = prompt('Во сколько обойдется?'),
-    appData = {
-        budget: money,
-        timeData: time,
-        expenses: {},
-        optionalExpenses:{},
-        income:[],
-        savings: false
-    };
+    time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
-appData.expenses[necessarily] = necessarilyMoney;
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses:{},
+    income:[],
+    savings: false
+};
+
+let exp1 = prompt('Введите обязательную статью расходов в этом месяц', ''),
+    exp2 = prompt('Во сколько обойдется?', ''),
+    exp3 = prompt('Введите обязательную статью расходов в этом месяц', ''),
+    exp4 = prompt('Во сколько обойдется?', '');
+
+appData.expenses[exp1] = exp2;
+appData.expenses[exp3] = exp4;
 
 console.log( appData.expenses );
 
-alert(`Бюджет на 1 день: ${(appData.budget - appData.expenses[necessarily]) / 30} `);
+alert(`Бюджет на 1 день: ${appData.budget / 30} `);
