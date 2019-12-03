879192d7cd138bfd7153dddb97381e1cf4ffeec1
Kostya <kvmatyushkin@gmail.com>
2019-12-03 14:18:29 +0300

    Redid expenses

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let money = prompt('Ваш бюджет на месяц?', ''),
-    time = prompt('Введите дату в формате YYYY-MM-DD', '');
+        time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses:{},
-    income:[],
-    savings: false
+        budget: money,
+        timeData: time,
+        expenses: {},
+        optionalExpenses:{},
+        income:[],
+        savings: false
 };
 
 let exp1 = prompt('Введите обязательную статью расходов в этом месяц', ''),
-    exp2 = prompt('Во сколько обойдется?', ''),
-    exp3 = prompt('Введите обязательную статью расходов в этом месяц', ''),
-    exp4 = prompt('Во сколько обойдется?', '');
+        exp2 = prompt('Во сколько обойдется?', ''),
+        exp3 = prompt('Введите обязательную статью расходов в этом месяц', ''),
+        exp4 = prompt('Во сколько обойдется?', '');
 
 appData.expenses[exp1] = exp2;
 appData.expenses[exp3] = exp4;
